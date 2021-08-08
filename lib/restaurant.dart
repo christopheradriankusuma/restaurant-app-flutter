00d647a4878ec570463deafbad10baebcd802eb3
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-08-09 00:04:39 +0700

    Add model and parsing data

---
+++ lib/restaurant.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+class Restaurant {
+  late String id;
+  late String name;
+  late String description;
+  late String pictureId;
+  late String city;
+  late num rating;
+  late Map<String, dynamic> menus;
+
+  Restaurant({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.pictureId,
+    required this.city,
+    required this.rating,
+    required this.menus,
+  });
+
+  Restaurant.fromJson(Map<String, dynamic> restaurant) {
+    id = restaurant['id'];
+    name = restaurant['name'];
+    description = restaurant['description'];
+    pictureId = restaurant['pictureId'];
+    city = restaurant['city'];
+    rating = restaurant['rating'];
+    menus = restaurant['menus'];
+  }
+
+  @override
+  String toString() {
+    return '$menus';
+  }
+}
+
+List<Restaurant> parseRestaurant(String? json) {
+  if (json == null) {
+    return [];
+  }
+
+  final List parsed = jsonDecode(json)['restaurants'];
+  // print(parsed);
+  return parsed.map((json) => Restaurant.fromJson(json)).toList();
+}
