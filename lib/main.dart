feebc88d02268225ed061ff47b08849b1752d1af
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-08-09 14:35:56 +0700

    Add splash screen when opening app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:restaurant_app/detail_page.dart';
 import 'package:restaurant_app/list_page.dart';
 import 'package:restaurant_app/restaurant.dart';
+import 'package:restaurant_app/splash_screen.dart';
 import 'package:restaurant_app/styles.dart';
 
 void main() => runApp(MyApp());
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Restaurant Application',
+      title: 'Dicoding Restaurant',
       theme: ThemeData(
         textTheme: myTextTheme,
         primaryColor: primaryColor,
@@ -18,9 +20,20 @@ class MyApp extends StatelessWidget {
         backgroundColor: backgroundColor,
         appBarTheme: AppBarTheme(backgroundColor: appBarColor),
       ),
-      initialRoute: ListPage.routeName,
+      initialRoute: SplashScreen.routeName,
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case ListPage.routeName:
+            return PageTransition(
+              child:
+                  ListPage(restaurants: settings.arguments as List<Restaurant>),
+              type: PageTransitionType.rightToLeft,
+              duration: Duration(milliseconds: 750),
+            );
+        }
+      },
       routes: {
-        ListPage.routeName: (context) => ListPage(),
+        SplashScreen.routeName: (context) => SplashScreen(),
         RestaurantDetail.routeName: (context) => RestaurantDetail(
               restaurant:
                   ModalRoute.of(context)?.settings.arguments as Restaurant,
