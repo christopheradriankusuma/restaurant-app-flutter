0e8fa0c55c4c31a3fc5920f369c294d2fecac23c
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-08-09 14:35:13 +0700

    Add menu in bottom sheet

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -3,64 +3,111 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:restaurant_app/restaurant.dart';
 
 class RestaurantDetail extends StatelessWidget {
-  static final String routeName = '/detail';
+  static const String routeName = '/detail';
   final Restaurant restaurant;
 
   RestaurantDetail({required this.restaurant});
 
+  List<TableRow> _createTable(context) {
+    List<TableRow> result = [];
+    int flen = restaurant.menus['foods'].length;
+    int dlen = restaurant.menus['drinks'].length;
+
+    for (int i = 0; i < (flen > dlen ? flen : dlen); ++i) {
+      result.add(
+        TableRow(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+              child: Text(
+                i < flen ? restaurant.menus['foods'][i]['name'] : '',
+                style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                      fontSize: 20,
+                      color: Color(0xFF1E5162),
+                    ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+              child: Text(
+                i < dlen ? restaurant.menus['drinks'][i]['name'] : '',
+                style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                      fontSize: 20,
+                      color: Color(0xFF1E5162),
+                    ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            leading: CircleAvatar(
-              backgroundColor: Colors.black87,
-              child: IconButton(
-                icon: Icon(Icons.arrow_back),
-                color: Colors.blue,
-                padding: const EdgeInsets.all(2),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
+      body: Container(
+        color: Theme.of(context).backgroundColor,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: CircleAvatar(
+                backgroundColor: Colors.black87,
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  color: Colors.blue,
+                  padding: const EdgeInsets.all(2),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
               ),
-            ),
-            leadingWidth: 44,
-            pinned: true,
-            expandedHeight: 200,
-            flexibleSpace: Stack(
-              children: [
-                Positioned.fill(
-                  child: Hero(
-                    tag: restaurant.pictureId,
-                    child: Image.network(
-                      restaurant.pictureId,
-                      fit: BoxFit.cover,
+              leadingWidth: 44,
+              pinned: true,
+              expandedHeight: 200,
+              flexibleSpace: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Hero(
+                      tag: restaurant.pictureId,
+                      child: Image.network(
+                        restaurant.pictureId,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, exception, stackTrace) {
+                          return Image.asset(
+                            'images/error.png',
+                            fit: BoxFit.cover,
+                          );
+                        },
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: SliverAppBarDelegate(
-              minHeight: 120,
-              maxHeight: 120,
-              child: FittedBox(
-                alignment: Alignment.centerLeft,
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: SliverAppBarDelegate(
+                minHeight: 120,
+                maxHeight: 120,
                 child: Padding(
                   padding:
                       const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        restaurant.name,
-                        style: Theme.of(context).textTheme.headline3!.copyWith(
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.bold,
-                            ),
+                      FittedBox(
+                        fit: BoxFit.contain,
+                        child: Text(
+                          restaurant.name,
+                          style:
+                              Theme.of(context).textTheme.headline3!.copyWith(
+                                    color: Theme.of(context).primaryColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
                       ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -99,13 +146,10 @@ class RestaurantDetail extends StatelessWidget {
                 ),
               ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                Container(
-                  color: Theme.of(context).backgroundColor,
-                  child: Padding(
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Padding(
                     padding:
                         const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                     child: Text(
@@ -113,33 +157,82 @@ class RestaurantDetail extends StatelessWidget {
                       style: Theme.of(context).textTheme.bodyText1,
                     ),
                   ),
-                ),
-                Container(
-                  color: Theme.of(context).backgroundColor,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Text(
-                      restaurant.description,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Theme.of(context).backgroundColor,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Text(
-                      restaurant.description,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Menu'),
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            backgroundColor: Colors.transparent,
+            builder: (context) {
+              return Container(
+                height: MediaQuery.of(context).size.height * 0.75,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).backgroundColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: const Radius.circular(25.0),
+                    topRight: const Radius.circular(25.0),
+                  ),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 6),
+                      child: Text(
+                        'Menu',
+                        style: Theme.of(context).textTheme.headline4!.copyWith(
+                              fontSize: 40,
+                              color: Colors.indigo[900],
+                            ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Foods',
+                            style:
+                                Theme.of(context).textTheme.headline4!.copyWith(
+                                      fontSize: 40,
+                                      color: Colors.indigo[900],
+                                    ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Expanded(
+                          child: Text(
+                            'Drinks',
+                            style:
+                                Theme.of(context).textTheme.headline4!.copyWith(
+                                      fontSize: 40,
+                                      color: Colors.indigo[900],
+                                    ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Table(
+                          // border: TableBorder.all(),
+                          children: _createTable(context),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
