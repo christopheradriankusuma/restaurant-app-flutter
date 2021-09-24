3718d0b7f32a94638dd9a8defce02eac2a445bd7
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-09-24 12:13:56 +0700

    Add settings page

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,42 @@
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
+import 'package:restaurant_app/common/navigation.dart';
 import 'package:restaurant_app/common/styles.dart';
 import 'package:restaurant_app/data/db/database_helper.dart';
 import 'package:restaurant_app/data/model/restaurant.dart';
+import 'package:restaurant_app/data/preferences/preferences_helper.dart';
 import 'package:restaurant_app/provider/database_provider.dart';
+import 'package:restaurant_app/provider/preferences_provider.dart';
+import 'package:restaurant_app/provider/scheduling_provider.dart';
 import 'package:restaurant_app/provider/search_provider.dart';
 import 'package:restaurant_app/ui/detail_page.dart';
 import 'package:restaurant_app/ui/favorite_page.dart';
 import 'package:restaurant_app/ui/list_page.dart';
+import 'package:restaurant_app/ui/settings_page.dart';
 import 'package:restaurant_app/ui/splash_screen.dart';
 import 'package:restaurant_app/ui/search_page.dart';
+import 'package:restaurant_app/utils/background_service.dart';
+import 'package:restaurant_app/utils/notification_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MyApp());
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final NotificationHelper _notificationHelper = NotificationHelper();
+  final BackgroundService _service = BackgroundService();
+
+  _service.initializeIsolate();
+  await AndroidAlarmManager.initialize();
+  await _notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,9 +51,20 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (_) => SearchProvider(),
         ),
+        ChangeNotifierProvider(
+          create: (_) => SchedulingProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => PreferencesProvider(
+            preferencesHelper: PreferencesHelper(
+              sharedPreferences: SharedPreferences.getInstance(),
+            ),
+          ),
+        ),
       ],
       child: MaterialApp(
         title: 'Dicoding Restaurant',
+        navigatorKey: navigatorKey,
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           textTheme: myTextTheme,
@@ -58,6 +93,7 @@ class MyApp extends StatelessWidget {
               ),
           SearchPage.routeName: (context) => SearchPage(),
           FavoritePage.routeName: (context) => FavoritePage(),
+          SettingsPage.routeName: (context) => SettingsPage(),
         },
       ),
     );
---
+++ lib/ui/settings_page.dart
@@ -0,0 +1,51 @@
+import 'package:restaurant_app/provider/preferences_provider.dart';
+import 'package:restaurant_app/provider/scheduling_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SettingsPage extends StatelessWidget {
+  static const String routeName = '/settings';
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PreferencesProvider>(
+      builder: (context, provider, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Settings',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline6!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+          ),
+          body: ListView(
+            children: [
+              Material(
+                child: ListTile(
+                  title: Text('Scheduling restaurant recommendation'),
+                  trailing: Consumer<SchedulingProvider>(
+                    builder: (context, scheduled, _) {
+                      scheduled
+                          .scheduleRestaurant(provider.isDailyRestaurantActive);
+                      return Switch(
+                        value: provider.isDailyRestaurantActive,
+                        onChanged: (value) async {
+                          scheduled.scheduleRestaurant(value);
+                          provider.enableDailyRestaurant(value);
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
