5b52e09a79d64cf0800ba88f31bff2c520558117
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-09-27 09:03:19 +0700

    Update unit testing using mockito

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -5,8 +5,9 @@ import 'package:restaurant_app/data/model/restaurant.dart';
 class ApiService {
   static final String _baseUrl = 'https://restaurant-api.dicoding.dev';
 
-  Future<RestaurantsResult> list() async {
-    final response = await http.get(Uri.parse(_baseUrl + "/list"));
+  Future<RestaurantsResult> list({http.Client? client}) async {
+    client ??= new http.Client();
+    final response = await client.get(Uri.parse(_baseUrl + "/list"));
     if (response.statusCode == 200) {
       return RestaurantsResult.fromJson(json.decode(response.body), "list");
     } else {
@@ -14,8 +15,10 @@ class ApiService {
     }
   }
 
-  Future<RestaurantsResult> detail({required String id}) async {
-    final response = await http.get(Uri.parse(_baseUrl + "/detail/$id"));
+  Future<RestaurantsResult> detail(
+      {required String id, http.Client? client}) async {
+    client ??= new http.Client();
+    final response = await client.get(Uri.parse(_baseUrl + "/detail/$id"));
     if (response.statusCode == 200) {
       return RestaurantsResult.fromJson(json.decode(response.body), "detail");
     } else {
@@ -23,7 +26,9 @@ class ApiService {
     }
   }
 
-  Future<RestaurantsResult> search({required String query}) async {
+  Future<RestaurantsResult> search(
+      {required String query, http.Client? client}) async {
+    client ??= new http.Client();
     query = Uri.encodeComponent(query);
     final response = await http.get(Uri.parse(_baseUrl + "/search?q=$query"));
     if (response.statusCode == 200) {
--- test/json_parsing_test.dart
+++ test/json_parsing_test.dart
@@ -1,12 +1,32 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:restaurant_app/data/api/api_service.dart';
 import 'package:restaurant_app/data/model/restaurant.dart';
 
+import 'json_parsing_test.mocks.dart';
+
+@GenerateMocks([http.Client])
 void main() {
-  test('should contains at least 1 restaurant', () async {
-    ApiService apiService = ApiService();
-    RestaurantsResult restaurantResult = await apiService.list();
-    bool result = restaurantResult.restaurants.length > 0;
-    expect(result, true);
-  });
+  test(
+    'should have at least 1 restaurants',
+    () async {
+      final client = MockClient();
+
+      when(
+        client.get(
+          Uri.parse('https://restaurant-api.dicoding.dev/list'),
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(
+            '{"error":false,"message":"success","count":20,"restaurants":[{"id":"rqdv5juczeskfw1e867","name":"Melting Pot","description":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.","pictureId":"14","city":"Medan","rating":4.2}]}',
+            200),
+      );
+
+      RestaurantsResult restaurantsResult = await ApiService().list();
+      bool result = restaurantsResult.restaurants.length > 0;
+      expect(result, true);
+    },
+  );
 }
